b7d2d7ad01ccbb966dc3d39de6a1975e1fdcb320
xodhksrjqnr <xodhksrjqnr@naver.com>
2023-06-18 23:05:07 +0900

    성별 무관 추가

--- src/component/js/filter/Sex.js
+++ src/component/js/filter/Sex.js
@@ -14,6 +14,13 @@ function Sex({setFilter}) {
         <div>
             <p>성별</p>
             <Button
+                id="무관"
+                className={cur === '무관' ? "activeButton" : "inactiveButton"}
+                onClick={() => setCondition('무관')}
+            >
+                무관
+            </Button>
+            <Button
                 id="남성"
                 className={cur === '남성' ? "activeButton" : "inactiveButton"}
                 onClick={() => setCondition('남성')}
